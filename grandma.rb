6d2681df6aedd8a563b048d677f6087d978c79d5
Mark Hernandez <m.hern1415@gmail.com>
2019-04-19 21:40:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,6 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-if speak_to_grandma == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-  elsif 
-end
+
+def speak_to_grandma(phrase)  
+  if "#{phrase}" == 
